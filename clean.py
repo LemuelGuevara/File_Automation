167d574940670299b8a4cc3efe99be51fdf0ef3f
RiderxL <Lemuelkuya@gmail.com>
2020-05-21 01:13:10 +0800

    Added the subcategories to the dict

--- clean.py
+++ clean.py
@@ -10,8 +10,8 @@ class MyOrganiser:
     def search(self, curPath):
         """
 
-        Walks the current directory and checks the file extensions if it maches
-        the values in the file_extensions dictionary
+        Walks the current directory and checks the file extensions 
+        if it maches the values in the file_extensions dictionary
         
         param: current working path
 
@@ -23,50 +23,53 @@ class MyOrganiser:
 
             self.moveFile(file_extension, new_file_name)
 
-    def makeSubDirs(self):
+    def makeSubDirs(self, dirs):
         """
 
-        Makes a directory first that represents its main category then makes subfolders
-        that represents its sub categories
+        Makes a directory first that represents its main category 
+        then makes subfolders that represents its sub categories
 
         """
-
-        self.subFolder = self.newPath
         
         try:
-            for folder in self.subfolder:
-                os.mkdir(self.subFolder)
+           os.makedirs(dirs)
         except Exception:
             pass
 
-    def moveFile(self, extension, file_format):
+    def moveFile(self, f_extension, file_format):
         """
 
-        Once the current directory is walked, it will begin check if the file 
-        extensions match the keys in the dictionary and if it does, it will return
-        the value of the key in the dictionary
+        Once the current directory is walked, it will begin check if 
+        the file extensions match the keys in the dictionary and if it does, 
+        it will return the value of the key in the dictionary
 
         param: file_extension, file_name_format
 
         """
     
         for category in file_extensions.items():
-            if extension in category:
-                self.newPath = os.path.join(self.dest, category[1])
-                self.filePrompt = file_format + str(category[1]) + '|'
+            if f_extension in category:
+                self.subDirs = os.path.join(self.dest, category[1])
+                self.filePrompt = file_format + '|' + str(category[1]) 
+                
+                self.makeSubDirs(self.subDirs)
 
-                shutil.move(file_format, self.newPath)
-                print(self.filePrompt)
-            
-            try:
-                if not extension in category:
-                    self.newPath = os.path.join(self.dest, file_extensions['none'])
-
-                    shutil.move(file_format, self.newPath)
+                try:
+                    shutil.move(file_format, self.subDirs)
                     print(self.filePrompt)
 
-            except Exception:
-                pass
+                except Exception:
+                    pass
+
+            """In proccess"""
+            # try:
+            #     if not extension in category:
+            #         self.subDirs = os.path.join(self.dest, file_extensions['none'])
+
+            #         shutil.move(file_format, self.subDirs)
+            #         print(file_format, str(category[1]), sep='|')
+            # except Exception:
+            #     pass
 
 if __name__ == "__main__":
     curPath = "/mnt/d/FILE TESTING/TEST ROOT"
@@ -74,5 +77,6 @@ if __name__ == "__main__":
 
     main = MyOrganiser()
     main.search(curPath)
+
 
 
--- extensions.py
+++ extensions.py
@@ -1,55 +1,57 @@
 
 file_extensions = {
 
-    #Text Files
-    '.doc' : 'DOCUMENTS/Text Files',
-    '.docx' : 'DOCUMENTS/Text Files',
-    '.txt' : 'DOCUMENTS/Text Files',
-    '.pdf' : 'DOCUMENTS/Text files',
+    #Text Files 
+    '.doc' : 'DOCUMENTS/Text Documents/Word Files',
+    '.docx' : 'DOCUMENTS/Text Documents/Word Files',
+    '.txt' : 'DOCUMENTS/Text Documents/Text Files',
+    '.pdf' : 'DOCUMENTS/Text Documents/PDF Files',
 
     #Data Files
-    '.csv' : 'DOCUMENTS/Data Files',
-    '.ppt' : 'DOCUMENTS/Data Files',
-    '.pptx' : 'DOCUMENTS/Data Files',
-    '.xls' : 'DOCUMENTS/Data Files',
-    '.xlsx' : 'DOCUMENTS/Data Files',
+    '.csv' : 'DOCUMENTS/Data Files/Excel Files',
+    '.ppt' : 'DOCUMENTS/Data Files/PowerPoint Files',
+    '.pptx' : 'DOCUMENTS/Data Files/PowerPoint Files',
+    '.xls' : 'DOCUMENTS/Data Files/Excel Files',
+    '.xlsx' : 'DOCUMENTS/Data Files/Excel Files',
 
     #Audio Files
-    '.mp4' : 'MEDIA/Audio Files',
-    '.mp3' : 'MEDIA/Audio Files',
-    '.wav' : 'MEDIA/Audio Files',
-    '.pkf' : 'MEDIA/Audio Files',
-    '.m4a' : 'MEDIA/Audio Files',
+    '.mp3' : 'MEDIA/Audio Files/MP3',
+    '.wav' : 'MEDIA/Audio Files/Adobe Audition',
+    '.pkf' : 'MEDIA/Audio Files/Adobe Audition',
+    '.m4a' : 'MEDIA/Audio Files/M4A',
 
-    '.mp4' : 'MEDIA/Video Files',
-    '.MOV' : 'MEDIA/Video Files',
-    '.avi' : 'MEDIA/Video Files',
+    #Video Files
+    '.mp4' : 'MEDIA/Video Files/MP4',
+    '.MOV' : 'MEDIA/Video Files/MOV',
+    '.avi' : 'MEDIA/Video Files/AVI',
 
     #Images Files
-    '.png' : 'MEDIA/Images Files',
-    '.jpg' : 'MEDIA/Images Files',
-    '.jpeg' : 'MEDIA/Images Files',
-    '.jfif' : 'MEDIA/Images Files',
+    '.png' : 'MEDIA/Image Files/PNG',
+    '.jpg' : 'MEDIA/Image Files/JPG',
+    '.jfif' : 'MEDIA/Image Files/JFIF',
+    '.jpeg' : 'MEDIA/Image Files/JPEG',
 
     #Vector and Photoshop Files
-    '.ai' : 'MEDIA/Vector and Photshop Files',
-    '.psd' : 'MEDIA/Vector and Photoshop Files',
-    '.xd' : 'MEDIA/Vector and Photoshop Files',
-    '.svg' : 'MEDIA/Vector and Photshop Files',
+    '.ai' : 'MEDIA/Adobe Image Related Files/Illustrator',
+    '.psd' : 'MEDIA/Adobe Image Related Files/Photoshop',
+    '.xd' : 'MEDIA/Adobe Image Related Files/XD',
+    '.svg' : 'MEDIA/Adobe Image Related Files/SVG',
+    '.eps' : 'MEDIA/Adobe Image Related Files/Illustrator',
 
     #Excecutable Files
-    '.exe' : 'INSTALLERS/Executable Files',
-    '.jar' : 'INSTALLERS/Executable Files',
-    '.bat' : 'INSTALLERS/Executable Files',
-    '.msi' : 'INSTALLERS/Executable Files',
+    '.exe' : 'INSTALLERS/Executable Files/EXE',
+    '.jar' : 'INSTALLERS/Executable Files/JAR',
+    '.bat' : 'INSTALLERS/Executable Files/BAT',
+    '.msi' : 'INSTALLERS/Executable Files/MSI',
 
     #Compressed Files
-    '.7z' : 'INSTALLERS/Compressed Files',
-    '.zip' : 'INSTALLERS/Compressed Files',
-    '.rar' : 'INSTALLERS/Compressed Files',
-    '.gz' : 'INSTALLERS/Compressed Files',
-    'tar.gz' : 'INSTALLERS/Compressed Files',
-    'tar.xz': 'INSTALLERS/Compressed Files',
+    '.7z' : 'INSTALLERS/Compressed Files/7z',
+    '.zip' : 'INSTALLERS/Compressed Files/ZIP',
+    '.rar' : 'INSTALLERS/Compressed Files/RAR',
+    '.gz' : 'INSTALLERS/Compressed Files/GZ',
+    'tar.gz' : 'INSTALLERS/Compressed Files/GZ',
+    '.xz' : 'INSTALLERS/Compressed Files/XZ',
+    'tar.xz': 'INSTALLERS/Compressed Files/XZ',
 
     #Disk Image Files
     '.iso' : 'INSTALLERS/Disk Images Files',
